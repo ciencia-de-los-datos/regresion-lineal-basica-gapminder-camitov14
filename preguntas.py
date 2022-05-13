993f0e5331ba83f873bb9426e0c939d1062c980d
camitov14 <102377531+camitov14@users.noreply.github.com>
2022-05-12 22:18:41 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -14,18 +14,18 @@ def pregunta_01():
     En este punto se realiza la lectura de conjuntos de datos.
     Complete el código presentado a continuación.
     """
+    import pandas as pd
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
     df = pd.read_csv("gm_2008_region.csv",sep=",")
 
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
     y = df["life"].values
     X = df["fertility"].values
-
+    y
     # Imprima las dimensiones de `y`
     print(np.shape(y))
 
     # Imprima las dimensiones de `X`
-    X.shape()
     print(np.shape(X))
 
     # Transforme `y` a un array de numpy usando reshape
